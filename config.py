40898f4b15b97f6196a10efab1427ca0a04887ce
george <georgii.mikriukov@tu-berlin.de>
2021-02-26 14:11:43 +0100

    .

--- config.py
+++ config.py
@@ -7,37 +7,13 @@ class DefaultConfig(object):
 
     pretrain_model_path = None  # './data/imagenet-vgg-f.mat'
 
+    flag = 'ucm'
+
     # visualization
     vis_env = None  # visdom env
     vis_port = 8097  # visdom port
 
-    # for ucm dataset
-    flag = 'ucm'
-    dataset = 'ucm'
-    data_path = './data/UCM_resnet18_bert_sum_12.h5'
-    db_size = 9450
-    num_label = 17
-    tag_dim = 768
     img_dim = 4096
-    query_size = 1050
-
-    # for flickr25k dataset
-    # dataset = 'flickr25k'
-    # data_path = './data/FLICKR-25K.mat'
-    # db_size = 18015
-    # num_label = 24
-    # tag_dim = 1386
-    # query_size = 2000
-
-    # for nus-wide dataset
-    # dataset = 'nus-wide'
-    # data_path = './data/dcmh-nus-wide'
-    # db_size = 193734
-    # num_label = 21
-    # tag_dim = 1000
-    # query_size = 2100
-
-    training_size = 7350
     batch_size = 128
     hidden_dim = 2048
     emb_dim = hidden_dim // 4
@@ -68,26 +44,61 @@ class DefaultConfig(object):
 
     proc = None
 
+    def data(self, flag):
+        if flag == 'mir':
+            self.dataset = 'flickr25k'
+            self.data_path = './data/FLICKR-25K.mat'
+            self.db_size = 18015
+            self.num_label = 24
+            self.query_size = 2000
+            self.tag_dim = 1386
+            self.training_size = 10000
+        if flag == 'nus':
+            self.dataset = 'nus-wide'
+            self.data_path = './data/NUS-WIDE-TC21.mat'
+            self.db_size = 193734
+            self.num_label = 21
+            self.query_size = 2100
+            self.tag_dim = 1000
+            self.training_size = 10000
+        if flag == 'ucm':
+            self.dataset = 'ucm'
+            self.data_path = './data/UCM_resnet18_bert_sum_12.h5'
+            self.db_size = 9450
+            self.num_label = 17
+            self.query_size = 1050
+            self.tag_dim = 768
+            self.training_size = 5250
+        if flag == 'rsicd':
+            self.dataset = 'rsicd'
+            self.data_path = '/home/george/Downloads/RSICD_resnet18_bert_sum_12.h5'
+            self.db_size = 52000
+            self.num_label = 31
+            self.query_size = 2605
+            self.tag_dim = 768
+            self.img_dim = 4096
+            self.training_size = 30000
+
     def parse(self, kwargs):
         """
         update configuration by kwargs.
         """
         for k, v in kwargs.items():
+            if k == 'flag':
+                self.data(v)
             if k == 'proc':
                 self.proc = v
             if k == 'device':
                 self.device = v
-            if k == 'flag':
-                self.flag = v
             if k == 'bit':
                 self.bit = int(v)
             if not hasattr(self, k):
-                warnings.warn("Waning: opt has no attribute %s" % k)
+                warnings.warn("Warning: opt has no attribute %s" % k)
             setattr(self, k, v)
 
         print('Configuration:')
         for k, v in self.__class__.__dict__.items():
-            if not k.startswith('__') and str(k) != 'parse':
+            if not k.startswith('__') and str(k) != 'parse' and str(k) != 'data':
                 print('\t{0}: {1}'.format(k, getattr(self, k)))
 
 
